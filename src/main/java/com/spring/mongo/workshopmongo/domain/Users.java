ed6c32d056db975933de3770f53622a89662c586
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2022-10-18 21:23:22 -0300

    Referenciando os posts do usuário

--- src/main/java/com/spring/mongo/workshopmongo/domain/Users.java
+++ src/main/java/com/spring/mongo/workshopmongo/domain/Users.java
@@ -1,10 +1,12 @@
 package com.spring.mongo.workshopmongo.domain;
 
 import java.io.Serializable;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="users")
@@ -16,6 +18,8 @@ public class Users implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true) //Faz com que os post relacionados só venham na minha consulta quando de fato acessá-los
+	private List<Post> posts = new ArrayList<>();
 	
 	public Users() {
 		
@@ -52,6 +56,15 @@ public class Users implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -69,6 +82,9 @@ public class Users implements Serializable{
 		Users other = (Users) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+
 	
 
 }
